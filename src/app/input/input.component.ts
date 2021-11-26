90678142622ea6704d41dfa7df28408effb0a5a5
Almontoya02 <42153887+Almontoya02@users.noreply.github.com>
2021-11-26 14:41:55 -0500

    inputs

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-input',
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputComponent implements OnInit {
 
+  @Input() inputType: string | undefined
+  @Input() placeHolder: string | undefined
+  @Input() img: string | undefined
   constructor() { }
 
   ngOnInit(): void {
